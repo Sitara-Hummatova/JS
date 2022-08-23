d9a81b8565c6c2cb8f46d03dde3f5a85b3d8cf93
Sitara-Hummatova <sarahummet6471@gmail.com>
2022-08-23 23:07:55 +0400

    finally founded

--- box.js
+++ box.js
@@ -2,14 +2,14 @@ const letsGo=document.querySelector('body')
 const createDiv=document.querySelector('button')
 
 const makeDivs=document.querySelector('.clicks')
-createDiv.addEventListener('click'. doIt)
+createDiv.addEventListener('click', doIt)
 
 function doIt(e) {
     e.preventDefault()
     
     const newDiv=document.createElement('div')
    
-    letsGo.appendChild(makeDivs)
+    letsGo.appendChild(newDiv)
     newDiv.classList.add('div-1')
 
 }
